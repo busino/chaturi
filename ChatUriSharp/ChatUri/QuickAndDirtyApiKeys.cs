7c0e431d36f8dd05a76c5d70a5ae7e208e1f111b
Peter Bucher <peter.bucher@sisag.ch>
2023-03-31 16:52:56 +0200

    Added Start/Stop record and updated Question-Answer style

--- ChatUriSharp/ChatUri/QuickAndDirtyApiKeys.cs
+++ ChatUriSharp/ChatUri/QuickAndDirtyApiKeys.cs
@@ -2,8 +2,8 @@ namespace ChatUri;
 
 public static class QuickAndDirtyApiKeys
 {
-    public static string AzureApiKey = string.Empty;
-    public static string OpenAiKey = string.Empty;
+    public static string AzureApiKey = "5553e80a837b4f0584f93878fdcc23d6";
+    public static string OpenAiKey = "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";
 
     public static bool KeysAreSet => !string.IsNullOrEmpty(AzureApiKey) && !string.IsNullOrEmpty(OpenAiKey);
 }
